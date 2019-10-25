a926187397e5257755e4c5558f0427a3c0d4c5e8
Suleyman TURKMEN <sturkmen@hotmail.com>
2019-10-25 23:48:18 +0300

    Update brightedgesexample.cpp

--- modules/ximgproc/samples/brightedgesexample.cpp
+++ modules/ximgproc/samples/brightedgesexample.cpp
@@ -45,59 +45,41 @@
 #include "opencv2/imgproc.hpp"
 #include "opencv2/imgcodecs.hpp"
 #include "opencv2/highgui.hpp"
+#include "opencv2/ximgproc.hpp"
 #include <stdio.h>
 #include <iostream>
-#include "opencv2/ximgproc.hpp"
+
 using namespace cv;
 using namespace ximgproc;
 using namespace std;
 
-static void help()
-{
-    printf("\nThis sample demonstrates BrightEdge detection\n"
-        "Call:\n"
-        "    /.edge [image_name -- Default is ../data/ml.png]\n\n");
-}
-const char* keys =
-{
-    "{help h||}{@image |../data/ml.png|input image name}"
-};
 int main(int argc, const char** argv)
 {
-    CommandLineParser parser(argc, argv, keys);
-    if (parser.has("help"))
-    {
-        help();
-        return 0;
-    }
-    string filename = parser.get<string>(0);
-    Mat image = imread(filename, IMREAD_COLOR);
+    CommandLineParser parser(argc, argv, "{@image |ml.png|input image name}");
+    parser.about("\nThis sample demonstrates usage of the function cv::ximgproc::BrightEdges()\n");
+    parser.printMessage();
+
+    Mat image = imread(samples::findFile(parser.get<string>(0)), IMREAD_COLOR);
     if (image.empty())
     {
-        printf("Cannot read image file: %s\n", filename.c_str());
-        help();
+        printf("Cannot read input image");
         return -1;
     }
-    // Create a window
-    // //  " original ";
-    namedWindow("Original");
+
+    //  " original ";
     imshow("Original", image);
     //  " absdiff ";
     Mat edge;
     BrightEdges(image, edge, 0); //  No contrast
-    namedWindow("Absolute Difference");
     imshow("Absolute Difference", edge);
     // " default contrast 1 ";
     BrightEdges(image, edge);
-    namedWindow("Default contrast");
     imshow("Default contrast", edge);// Default contrast 1
     // " Contrast 5  \n";
     BrightEdges(image, edge, 5);
-    namedWindow("Contrast 5");
     imshow("Contrast 5", edge);
     // " Contrast 10  \n";
     BrightEdges(image, edge, 10);
-    namedWindow("Contrast 10");
     imshow("Contrast 10", edge);
     //  "wait key ";
     waitKey(0);
