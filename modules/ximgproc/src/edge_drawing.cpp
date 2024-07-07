b62598b4bbc13d175af08b0bfe6c8efdd83981cc
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-07-08 01:15:11 +0300

    Update edge_drawing.cpp

--- modules/ximgproc/src/edge_drawing.cpp
+++ modules/ximgproc/src/edge_drawing.cpp
@@ -782,7 +782,7 @@ exit(0);
                             r++;
                         c--;
                     }
-
+if (srcImage.type() == CV_8UC3) printf("(%dx%d)\n", r, c);
                     if (edgeImg[r * width + c] == EDGE_PIXEL || gradImg[r * width + c] < gradThresh)
                     {
                         if (chainLen > 0)
