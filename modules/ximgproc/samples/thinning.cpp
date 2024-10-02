18d7e668c863f8beaacdf9f62a9d8849566f50b6
Suleyman TURKMEN <sturkmen@hotmail.com>
2024-10-02 17:33:46 +0300

    Update thinning.cpp

--- modules/ximgproc/samples/thinning.cpp
+++ modules/ximgproc/samples/thinning.cpp
@@ -1,45 +1,224 @@
+
+#include <opencv2/core.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/videoio.hpp>
+#include <opencv2/imgproc.hpp>
+
+#include <fstream>
 #include <iostream>
-
-#include "opencv2/imgproc.hpp"
-#include "opencv2/highgui.hpp"
-
-#include "opencv2/ximgproc.hpp"
+#include <map>
 
 using namespace std;
 using namespace cv;
 
-int main()
+// look up table - there is one entry for each of the 2^8=256 possible
+// combinations of 8 binary neighbors.
+static uint8_t lut_zhang_iter0[] = {
+    1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+    0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+    1, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+    1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0,
+    1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 0, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1};
+
+static uint8_t lut_zhang_iter1[] = {
+    1, 1, 1, 0, 1, 1, 0, 0, 1, 1, 1, 1, 0, 1,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1,
+    0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1,
+    0, 1, 1, 0, 1, 0, 1, 1, 1, 0, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 1, 0,
+    1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 0, 0, 1, 1,
+    0, 1, 1, 1};
+
+static uint8_t lut_guo_iter0[] = {
+    1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 0, 0, 1, 1,
+    0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 0, 0,
+    0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 0, 1,
+    0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 0, 0, 0, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1, 0, 1,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 0, 1, 0, 1,
+    0, 1, 1, 1, 1, 0, 1, 1, 1, 0, 1, 1, 1, 0,
+    1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 0, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1};
+
+static uint8_t lut_guo_iter1[] = {
+    1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 1, 1, 1, 1, 1, 1, 1, 0, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1, 0, 0,
+    1, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0, 0, 1, 1,
+    1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 0, 0, 0,
+    1, 1, 0, 0, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1,
+    0, 0, 0, 0, 1, 1, 0, 0, 1, 1, 1, 1, 1, 1,
+    1, 1, 0, 0, 0, 0, 1, 1, 1, 1, 0, 0, 1, 1,
+    1, 1, 1, 1};
+
+// Applies a thinning iteration to a binary image
+static void thinningIteration(Mat &img, Mat &marker, const uint8_t* const lut) {
+    int rows = img.rows;
+    int cols = img.cols;
+    marker.col(0).setTo(1);
+    marker.col(cols - 1).setTo(1);
+    marker.row(0).setTo(1);
+    marker.row(rows - 1).setTo(1);
+
+    marker.forEach<uchar>([=](uchar& value, const int position[]) {
+        int i = position[0];
+        int j = position[1];
+        if (i == 0 || j == 0 || i == rows - 1 || j == cols - 1) { return; }
+
+        auto ptr = img.ptr(i, j); // p1
+        if (ptr[0]) {
+            uchar p2 = ptr[-cols] != 0;
+            uchar p3 = ptr[-cols + 1] != 0;
+            uchar p4 = ptr[1] != 0;
+            uchar p5 = ptr[cols + 1] != 0;
+            uchar p6 = ptr[cols] != 0;
+            uchar p7 = ptr[cols - 1] != 0;
+            uchar p8 = ptr[-1] != 0;
+            uchar p9 = ptr[-cols - 1] != 0;
+
+            int neighbors = p9 | (p2 << 1) | (p3 << 2) | (p4 << 3) | (p5 << 4) | (p6 << 5) | (p7 << 6) | (p8 << 7);
+            value = 1;
+        }
+    });
+
+    img &= marker;
+    marker.setTo(0);
+}
+
+// Apply the thinning procedure to a given image
+void thinning(InputArray input, OutputArray output, int thinningType){
+    Mat processed = input.getMat().clone();
+    CV_CheckTypeEQ(processed.type(), CV_8UC1, "");
+    // Enforce the range of the input image to be in between 0 - 255
+    processed /= 255;
+    Mat prev = processed.clone();
+    Mat marker = Mat::zeros(processed.size(), CV_8UC1);
+    const auto lutIter0 = (thinningType == 1) ? lut_guo_iter0 : lut_zhang_iter0;
+    const auto lutIter1 = (thinningType == 1) ? lut_guo_iter1 : lut_zhang_iter1;
+    do {
+        thinningIteration(processed, marker, lutIter0);
+        thinningIteration(processed, marker, lutIter1);
+        const auto res = cv::norm(processed, prev, cv::NORM_L1);
+        if (res <= 0) { break; }
+        processed.copyTo(prev);
+    } while (true);
+
+    processed *= 255;
+    output.assign(processed);
+}
+
+int main(int argc, const char** argv)
 {
-    Mat img = imread("opencv-logo.png", IMREAD_COLOR);
-    resize(img, img, Size(), 0.5, 0.5, INTER_LINEAR_EXACT);
+   // for (int i = 0; i < 256; i++)
+  //  if (lut_zhang_iter1[i] == 0)
+    //    printf("%d\n", i);
+    Mat src = imread("08.png", IMREAD_GRAYSCALE);
+    Mat dst,check_img;
+    TickMeter tm;
+    tm.start();
+    thinning(~src, dst, 0);
+    tm.stop();
+    cout << tm << endl;
 
-    /// Threshold the input image
-    Mat img_grayscale, img_binary;
-    cvtColor(img, img_grayscale,COLOR_BGR2GRAY);
-    threshold(img_grayscale, img_binary, 0, 255, THRESH_OTSU | THRESH_BINARY_INV);
+    tm.reset();
+    tm.start();
+    thinning(src, dst, 0);
+    tm.stop();
+    cout << tm << endl;
 
-    /// Apply thinning to get a skeleton
-    Mat img_thinning_ZS, img_thinning_GH;
-    ximgproc::thinning(img_binary, img_thinning_ZS, ximgproc::THINNING_ZHANGSUEN);
-    ximgproc::thinning(img_binary, img_thinning_GH, ximgproc::THINNING_GUOHALL);
+    tm.reset();
+    tm.start();
+    thinning(~src, dst, 0);
+    tm.stop();
+    cout << tm << endl;
 
-    /// Make 3 channel images from thinning result
-    Mat result_ZS(img.rows, img.cols, CV_8UC3), result_GH(img.rows, img.cols, CV_8UC3);
+    tm.reset();
+    tm.start();
+    thinning(src, dst, 0);
+    tm.stop();
+    cout << tm << endl;
 
-    Mat in[] = { img_thinning_ZS, img_thinning_ZS, img_thinning_ZS };
-    Mat in2[] = { img_thinning_GH, img_thinning_GH, img_thinning_GH };
-    int from_to[] = { 0,0, 1,1, 2,2 };
-    mixChannels( in, 3, &result_ZS, 1, from_to, 3 );
-    mixChannels( in2, 3, &result_GH, 1, from_to, 3 );
+    check_img = imread("Thinning_ZHANGSUEN.png", IMREAD_GRAYSCALE);
+    cout << cv::norm(check_img, dst, cv::NORM_L1) << endl;
+    imshow("dst", dst);
+    waitKey();
+    thinning(~src, dst, 0);
 
-    /// Combine everything into a canvas
-    Mat canvas(img.rows, img.cols * 3, CV_8UC3);
-    img.copyTo( canvas( Rect(0, 0, img.cols, img.rows) ) );
-    result_ZS.copyTo( canvas( Rect(img.cols, 0, img.cols, img.rows) ) );
-    result_GH.copyTo( canvas( Rect(img.cols*2, 0, img.cols, img.rows) ) );
+    check_img = imread("Thinning_inv_ZHANGSUEN.png", IMREAD_GRAYSCALE);
+    cout << cv::norm(check_img, dst, cv::NORM_L1) << endl;
+    imshow("dst", dst);
+    waitKey(100);
+        thinning(src, dst, 1);
 
-    /// Visualize result
-    imshow("Skeleton", canvas); waitKey(0);
+    check_img = imread("Thinning_GUOHALL.png", IMREAD_GRAYSCALE);
+    cout << cv::norm(check_img, dst, cv::NORM_L1) << endl;
+        imshow("dst", dst);
+    waitKey(100);
+    thinning(~src, dst, 1);
 
+    check_img = imread("Thinning_inv_GUOHALL.png", IMREAD_GRAYSCALE);
+    cout << cv::norm(check_img, dst, cv::NORM_L1) << endl;
+
+    imshow("dst", dst);
+    waitKey(100);
+        exit(0);
+    vector<Mat> imgs;
+    Mat img(100, 100, CV_8UC1);
+    imgs.push_back(img);
+    std::vector<uchar> buf;
+    bool ret_image = imencode(".tiff", imgs, buf);
+    bool ret_multipage = imencode(".tiff", imgs, buf);
+    printf("%d\n", ret_image);
+    printf("%d\n", ret_multipage);
+    imgs.push_back(img.clone());
+    ret_multipage = imencode(".tiff", imgs, buf);
+    printf("%d\n", ret_multipage);
     return 0;
 }
